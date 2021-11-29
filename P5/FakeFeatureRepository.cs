84cab3714c019e12044aa387928f0549a16a6b4b
Sam Resop <samuel.resop@trojans.dsu.edu>
2021-11-29 13:16:33 -0600

    Added repostories, classes, and interfaces for Requirements and Features.

---
+++ P5/FakeFeatureRepository.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P5
+{
+    public class FakeFeatureRepository : IFeatureRepository
+    {
+        public string NO_ERROR = "";
+        public string DUPLICATE_TITLE_ERROR = "Title must be unique";
+        public string EMPTY_TITLE_ERROR = "Title must have a value";
+        public string NOT_FOUND_ERROR = "Feature not found";
+        public string INVALID_PROJECT_ID_ERROR = "Invalid Project Id for Feature";
+
+        private static List<Feature> features = new List<Feature>();
+
+        public string Add(Feature feature)
+        {
+            return null;
+        }
+
+        public List<Feature> GetAll(int ProjectId)
+        {
+            return null;
+        }
+
+        public string Remove(Feature feature)
+        {
+            return null;
+        }
+        public string Modify(Feature feature)
+        {
+            return null;
+        }
+        public Feature GetFeatureById(int projectId, int featureId)
+        {
+            return null;
+        }
+        public Feature GetFeatureByTitle(int projectId, string title)
+        {
+            return null;
+        }
+    }
+}
---
+++ P5/FakeRequirementRepository.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P5
+{
+    public class FakeRequirementRepository : IRequirementRepository
+    {
+        public string NO_ERROR = "";
+        public string DUPLICATE_STATEMENT_ERROR = "Statements must be unique";
+        public string EMPTY_STATEMENT_ERROR = "Statments must have a value";
+        public string REQUIREMENT_NOT_FOUND_ERROR = "Requirement does not exist";
+        public string MISSING_FEATUREID_ERROR = "Must select a feature for this requirement";
+        public string MISSING_PROJECTID_ERROR = "Must select a project for this requirement";
+
+
+        private static List<Requirement> requirements = new List<Requirement>();
+
+        public string Add(Requirement requirement)
+        {
+            return null;
+        }
+        public List<Requirement> GetAll(int ProjectId)
+        {
+            return null;
+        }
+        public string Remove(Requirement requirement)
+        {
+            return null;
+        }
+        public string Modify(Requirement requirement)
+        {
+            return null;
+        }
+        public Requirement GetRequirementById(int requirementId)
+        {
+            return null;
+        }
+        public int CountByFeatureId(int featureId)
+        {
+            return 0;
+        }
+        public void RemoveByFeatureId(int featureId)
+        {
+
+        }
+    }
+}
